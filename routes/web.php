b625e390e9a49c6e779191c1e842e94118bad365
roboknob <roboknob1@yandex.ru>
2025-01-28 22:50:31 +0300

    inital

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/healthcheck', function () {
+    return response()->json(['status' => 'ok'], 200);
+});
