b0001d2cfb8ff0485781c44addc11d1f44b6321a
roboknob <roboknob1@yandex.ru>
2025-01-28 23:03:46 +0300

    healthcheck

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/healthcheck', function () {
     return response()->json(['status' => 'ok'], 200);
 });
